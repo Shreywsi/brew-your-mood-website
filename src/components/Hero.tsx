0dcd50027ace9c70aaf2e879669906d92df16f02
Shreywsi <b230029@nitsikkim.ac.in>
2025-06-29 12:08:58 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = ({ onStartQuiz }: HeroProps) => {
       <div 
         className="absolute inset-0 bg-gradient-to-br from-mocha-900/20 via-caramel-800/30 to-mocha-800/40"
         style={{
-          backgroundImage: `url('https://images.unsplash.com/photo-1721322800607-8c38375eef04?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2000&q=80')`,
+          backgroundImage: `url('/images/hero-image.jpg')`, 
           backgroundSize: 'cover',
           backgroundPosition: 'center',
         }}
