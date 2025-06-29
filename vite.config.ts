7c73ae4a9c4394058721d55e5e9358922c4df097
Shreywsi <b230029@nitsikkim.ac.in>
2025-06-29 11:33:16 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/brew-your-mood-website/', 
   plugins: [react()],
   resolve: {
     alias: {
